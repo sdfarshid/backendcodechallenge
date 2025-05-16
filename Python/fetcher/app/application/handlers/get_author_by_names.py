4fb515e920dd90153a063e763616eaffd611917e
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-16 15:46:39 +0330

    Feat (fetcher): implement handler to get authors by name

---
+++ Python/fetcher/app/application/handlers/get_author_by_names.py
@@ -0,0 +1,24 @@
+from fastapi import Depends
+
+from app.application.queries.get_authors_by_name import GetAuthorsByNamesQuery
+from app.domain.interface.Iauthor_repository import IAuthorRepository
+from app.infrastructure.repositories.author_repository import AuthorRepository
+from app.utilities.log import commit_logger
+
+
+class GetAuthorsByNamesCommandHandler:
+
+    def __init__(self, author_repository: IAuthorRepository = Depends(AuthorRepository)):
+        self.repository = author_repository
+        self.logger = commit_logger
+
+
+    async def handle(self, query: GetAuthorsByNamesQuery) -> dict:
+        try:
+            existing_authors = await self.repository.get_authors_by_names(query.unique_author_names)
+            return {author.name: author.id for author in existing_authors}
+        except Exception as e:
+            self.logger.error(f"Failed to store  GetAuthorByNamesCommandHandler : {e}")
+            raise RuntimeError(f"Failed fetching users : {e}")
+
+
