a9f108537896431e922cc88772cdf5d4d7fc3011
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-16 05:47:35 +0330

    Refactor (fetcher): add PG_UUID to table columns

--- Python/fetcher/app/infrastructure/database/models/author.py
+++ Python/fetcher/app/infrastructure/database/models/author.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column,String
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from app.infrastructure.database.session import Base
+
+
 
 
 class AuthorModel(Base):
     __tablename__ = "authors"
-    id = Column(Integer, primary_key=True)
+    id = Column(PG_UUID(as_uuid=True), primary_key=True)
     name = Column(String(255), unique=True, nullable=False, index=True)
--- Python/fetcher/app/infrastructure/database/models/commit.py
+++ Python/fetcher/app/infrastructure/database/models/commit.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+
 
 from app.infrastructure.database.session import Base
 
 
 class CommitModel(Base):
     __tablename__ = "commits"
-    id = Column(Integer, primary_key=True)
+    id = Column(PG_UUID(as_uuid=True), primary_key=True)
     hash = Column(String, unique=True, nullable=False)
-    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
+    author_id = Column(PG_UUID(as_uuid=True), ForeignKey("authors.id"), nullable=False, index=True)
     created_at = Column(DateTime, default=datetime.now)
 
     author = relationship("AuthorModel", backref="commits")
