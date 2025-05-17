5d1a3ccb3f891a02a2a67b9dc067e02f4ba7c184
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-17 13:42:37 +0330

    Feat (fetcher): implement fetcher service to orchestration to fetching and storing commits

--- Python/fetcher/app/api/v1/endpoint/fetch.py
+++ Python/fetcher/app/api/v1/endpoint/fetch.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from app.application.commands.fetch_commits import FetchCommitsCommand
+from app.application.services.fetcher_service import FetcherService
+from app.utilities.helper import handle_exceptions
 
 router = APIRouter()
 
+ServiceDependency = Annotated[FetcherService, Depends(FetcherService)]
+
+
 
 @router.post("/fetch")
-async def fetching_commits(command: FetchCommitsCommand):
-    pass
+@handle_exceptions
+async def fetching_commits(command: FetchCommitsCommand, service: ServiceDependency):
+     return await service.fetch_and_store_commits(command=command)
