5557c4ad65f9cc815bf944f4f29e21396e8d0f68
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-17 19:44:04 +0330

    Feat (fetcher): implement Author service

---
+++ Python/fetcher/app/application/services/author_service.py
@@ -0,0 +1,27 @@
+from typing import List, Dict
+
+from fastapi import Depends
+
+from app.application.commands.crete_author import CreateAuthorCommand
+from app.application.queries.get_authors_by_name import GetAuthorsByNamesQuery
+
+
+
+class AuthorService:
+    def __init__(self, get_author_by_name_handler, create_author_handler):
+        self.get_author_by_name_handler = get_author_by_name_handler
+        self.create_author_handler = create_author_handler
+
+    async def manage_authors(self, all_commits: list) -> Dict:
+        unique_author_names = list(set(commit["author"].strip() for commit in all_commits))
+        existing_author_map = await self.get_authors_by_names(unique_author_names)
+        return await self.create_authors(unique_author_names, existing_author_map)
+
+
+    async def get_authors_by_names(self, names: List[str]):
+        query = GetAuthorsByNamesQuery(unique_author_names=names)
+        return await self.get_author_by_name_handler.handle(query)
+
+    async def create_authors(self, names: List[str], existing_author_map: Dict):
+        command = CreateAuthorCommand(unique_author_names=names, existing_author_map=existing_author_map)
+        return await self.create_author_handler.handle(command)
