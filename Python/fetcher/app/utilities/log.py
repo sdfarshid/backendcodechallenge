a96a000dd60d0da7076cb215badf26d7270a250f
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-17 19:30:17 +0330

    Feat (fetcher): add separate loggers for fetcher, commit, and core services

--- Python/fetcher/app/utilities/log.py
+++ Python/fetcher/app/utilities/log.py
@@ -12,8 +12,6 @@ log_dir.mkdir(parents=True, exist_ok=True)
 logger = logging.getLogger("app")
 logger.setLevel(logging.DEBUG)
 
-commit_logger = logging.getLogger("commit_logger")
-commit_logger.setLevel(logging.INFO)
 
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -21,17 +19,41 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 file_handler = logging.FileHandler(log_dir / "app.log", encoding="utf-8")
 file_handler.setFormatter(formatter)
 
+
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(formatter)
-
-
-
 if not logger.hasHandlers():
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
+
+
+commit_logger = logging.getLogger("commit_logger")
+commit_logger.setLevel(logging.INFO)
+
+file_commit_logger = logging.FileHandler(log_dir / "commit_logger.log", encoding="utf-8")
+file_commit_logger.setFormatter(formatter)
+
+
 if not commit_logger.hasHandlers():
-    commit_logger.addHandler(file_handler)
+    commit_logger.addHandler(file_commit_logger)
+    commit_logger.addHandler(stream_handler)
+
+
+
+
+fetcher_logger = logging.getLogger("fetcher_logger")
+fetcher_logger.setLevel(logging.INFO)
+
+
+file_fetcher_logger = logging.FileHandler(log_dir / "fetcher_logger.log", encoding="utf-8")
+file_fetcher_logger.setFormatter(formatter)
+
+
+
+if not fetcher_logger.hasHandlers():
+    fetcher_logger.addHandler(file_fetcher_logger)
+    fetcher_logger.addHandler(stream_handler)
 
 
 def DebugWaring(message: object) -> object:
