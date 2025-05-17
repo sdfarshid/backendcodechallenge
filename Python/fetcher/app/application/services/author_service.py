e0dfb30c0e0443ee13ef667e06645d1d1540d783
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-17 21:54:37 +0330

    feat (fetcher): implement fetching list of authors in service

--- Python/fetcher/app/application/services/author_service.py
+++ Python/fetcher/app/application/services/author_service.py
@@ -1,16 +1,20 @@
 from typing import List, Dict
 
-from fastapi import Depends
 
 from app.application.commands.crete_author import CreateAuthorCommand
 from app.application.queries.get_authors_by_name import GetAuthorsByNamesQuery
-
+from app.application.queries.list_authors import ListAuthorsQuery
+from app.domain.aggregates.author import Author
 
 
 class AuthorService:
-    def __init__(self, get_author_by_name_handler, create_author_handler):
+    def __init__(self,
+                 get_author_by_name_handler,
+                 get_list_authors_handler,
+                 create_author_handler):
         self.get_author_by_name_handler = get_author_by_name_handler
         self.create_author_handler = create_author_handler
+        self.list_handler = get_list_authors_handler
 
     async def manage_authors(self, all_commits: list) -> Dict:
         unique_author_names = list(set(commit["author"].strip() for commit in all_commits))
@@ -25,3 +29,8 @@ class AuthorService:
     async def create_authors(self, names: List[str], existing_author_map: Dict):
         command = CreateAuthorCommand(unique_author_names=names, existing_author_map=existing_author_map)
         return await self.create_author_handler.handle(command)
+
+
+    async def list_authors(self, query: ListAuthorsQuery) -> List[Author]:
+        return await self.list_handler.handle(query)
+
