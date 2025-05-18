9db76be10361806b18bd41c7ec924e001b68ca24
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-18 04:52:21 +0330

    Refactor (fetcher): create a backgroundTask for fetching data with call api in front side

--- Python/fetcher/app/api/v1/endpoint/fetch.py
+++ Python/fetcher/app/api/v1/endpoint/fetch.py
@@ -1,6 +1,4 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 
 from app.application.commands.fetch_commits import FetchCommitsCommand
 from app.application.services.fetcher_service import FetcherService
@@ -20,3 +18,15 @@ async def fetching_commits(
     service: FetcherService = Depends(get_fetcher_service)
 ):
      return await service.fetch_and_store_commits(command=command)
+
+
+@router.post("/back-fetching")
+@handle_exceptions
+async def back_fetching_commits(
+    command: FetchCommitsCommand,
+    background_tasks: BackgroundTasks,
+    service: FetcherService = Depends(get_fetcher_service)
+):
+    background_tasks.add_task(service.fetch_and_store_commits, command)
+    return {"message": f"Fetch started in background (count: {command.count})"}
+
