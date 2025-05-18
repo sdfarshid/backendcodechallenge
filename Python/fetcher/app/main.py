b98034ed97623339142904551b1de2a655090f21
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-18 03:49:35 +0330

    Feat (fetcher): add Jinja2Templates so that creating view

--- Python/fetcher/app/main.py
+++ Python/fetcher/app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 from app.api.v1.routers import api_router
 from app.utilities.log import logger, DebugWaring
+
+
+
+
 
 app = FastAPI(
     title="Fetcher Service",
@@ -10,18 +18,25 @@ app = FastAPI(
     debug=True
 )
 
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = BASE_DIR / "templates"
+STATIC_DIR = BASE_DIR / "static"
+
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
+
 
 app.include_router(api_router, prefix="/api/v1")
-
 
 @app.on_event("startup")
 async def startup():
     logger.info("Service started")
 
 
-
 @app.get("/")
-async def root():
-    DebugWaring("Root endpoint accessed")
-    return {"message": "Hello Fetcher Service!"}
-
+async def home(request: Request):
+    data = {
+        "title": "welcome",
+        "header": "welcome",
+    }
+    return templates.TemplateResponse("index.html", {"request": request, **data})
